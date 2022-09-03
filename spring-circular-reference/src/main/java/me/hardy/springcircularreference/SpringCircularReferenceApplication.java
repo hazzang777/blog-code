d87eac14e97dd8b1515ef5a6a25a7a5d24077340
hardy <hardy@hardyui-MacBookAir.local>
2022-09-03 17:55:17 +0900

    project: spring-circular-reference

---
+++ spring-circular-reference/src/main/java/me/hardy/springcircularreference/SpringCircularReferenceApplication.java
@@ -0,0 +1,13 @@
+package me.hardy.springcircularreference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCircularReferenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCircularReferenceApplication.class, args);
+    }
+
+}
---
+++ spring-circular-reference/src/test/java/me/hardy/springcircularreference/SpringCircularReferenceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hardy.springcircularreference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCircularReferenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
